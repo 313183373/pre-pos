941005e5489fa945048e55768741e49338f50692
313183373 <313183373@qq.com>
2017-07-21 21:38:57 +0800

    section 3 practice 3 finished

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,23 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let map={};
+  for(let i=0;i<collectionA.length;i++){
+    if(typeof(map[collectionA[i]])=="undefined"){
+      map[collectionA[i]]=1;
+    }else{
+      map[collectionA[i]]++;
+    }
+  }
+  for(let item in map){
+    if(objectB.value.indexOf(item)!=-1){
+      let cnt=Math.floor(map[item]/3);
+      map[item]-=cnt;
+    }
+  }
+  let ret=[];
+  for(let item in map){
+    ret.push({key:item,count:map[item]});
+  }
+  return ret;
 }
