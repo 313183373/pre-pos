8605d735de1f0626049a506a204f7e7c0a8dfa6c
313183373 <313183373@qq.com>
2017-07-21 20:58:16 +0800

    section 2 practice 2 finished

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,25 @@
 'use strict';
 
+
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+	let dic={};
+	let ans=[];
+	for(let i=0;i<collection.length;i++){
+		if(collection[i].length>1){
+			var c=collection[i].split('-')[0];
+			var cnt=collection[i].split('-')[1]-'0';
+		}else{
+			var c=collection[i];
+			var cnt=1;
+		}
+		if(typeof(dic[c])=='undefined'){
+			dic[c]=cnt;
+		}else{
+			dic[c]+=cnt;
+		}
+	}
+	for(let item in dic){
+		ans.push({key:item,count:dic[item]})
+	}
+	return ans;
 }
