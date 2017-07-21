460dc4796a5f03ee6904df962afe4af150d8a05f
313183373 <313183373@qq.com>
2017-07-21 20:21:14 +0800

    section-3 finished

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,5 +1,11 @@
 'use strict';
 
-function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+function collectSameElements(collectionA, collectionB) {
+    let ret=[];
+    for(let i=0;i<collectionA.length;i++){
+        if(collectionB.value.indexOf(collectionA[i])!=-1){
+            ret.push(collectionA[i]);
+        }
+    }
+    return ret;
 }
